34583361ea8863ce0e4e25e98d846c69c496fba8
Grigorov- <Grigorov-@users.noreply.github.com>
2025-09-30 21:00:50 +0300

    Update EmailConnector.java

--- src/main/java/com/the/good/club/core/connector/EmailConnector.java
+++ src/main/java/com/the/good/club/core/connector/EmailConnector.java
@@ -1,6 +1,7 @@
 package com.the.good.club.core.connector;
 
 import jakarta.mail.MessagingException;
+import java.nio.charset.StandardCharsets;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
